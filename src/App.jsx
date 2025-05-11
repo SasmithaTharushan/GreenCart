2312086e6d473e22142292110003aa90cc881604
Codex_killer <imsasmithatharushan32@gmail.com>
2025-05-11 12:47:37 +0530

    Initial commit - front end mostly done

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { use } from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import { useLocation } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer'
+import { useAppContext } from './context/AppContext';
+import Login from './components/Login';
+import AllProducts from './pages/AllProducts';
+import ProductsCategory from './pages/ProductsCategory';
+
+const App = () => {
+
+  const isSellerPath = useLocation().pathname.includes("seller");
+  const {showUserLogin} = useAppContext();
+  return (
+    <div>
+
+      {isSellerPath ? null : <Navbar/> }
+      {showUserLogin ? <Login/> : null}
+
+      <Toaster />
+
+      <div className={`${isSellerPath ? "" :  "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+        <Routes>
+           <Route path='/' element={<Home/>} />
+           <Route path='/products' element={<AllProducts/>} />
+            <Route path='/products/:category' element={<ProductsCategory/>} />
+          </Routes>
+      </div>
+    {!isSellerPath && <Footer/>}
+    </div>
+  )
+}
+
+export default App
